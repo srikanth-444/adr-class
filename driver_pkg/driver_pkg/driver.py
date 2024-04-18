7261e1ebc0d572efcb88039d5ec1914aa823644e
srikanth <srikanth.popuri4444@gmail.com>
2024-04-18 13:40:11 -0600

    added logic for turn

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -1,7 +1,8 @@
+import numpy as np
 class Driver():
     def __init__(self) -> None:
-        self.distance_matrix=[]
-        self.throttle=0.0
+        self.distance_matrix=np.argmax([])
+        self.throttle=0.5
         self.angle=0.0
 
     def get_throttle(self):
@@ -18,12 +19,31 @@ class Driver():
 
 
     def get_controls(self,distance_matrix):
-        self.distance_matrix=[]
+        self.distance_matrix=distance_matrix
         
-
+        left_distances=self.distance_matrix[1:self.distance_matrix.size//2-1]
+        right_distances=self.distance_matrix[self.distance_matrix.size//2+1 :]
         # logic start here 
-
+        e=self.scan_for_turn(left_distances,right_distances)
+        if e>0:
+            self.angle=float(e/90)
+        else:
+            self.angle=0
         
 
         return self.throttle
-   
+    def scan_for_turn(self,left_distances,right_distances)-> int:
+        left=left_distances[30:90]
+        left_max_distance=left[np.argmax(left)]
+        right=right_distances[30:90]
+        right_max_distance=right[np.argmax(right)]
+
+
+        if (left_max_distance>right_max_distance and left_max_distance>=0.5):
+                e=30+np.argmax(left)
+                return -e
+        elif(right_max_distance>left_max_distance and right_max_distance>=0.5):
+                e=30+np.argmax(right)
+                return e
+        else:
+            return 0
