7748d3e62c33a2dca2571915f6f9f18b14c7426e
srikanth <srikanth.popuri4444@gmail.com>
2024-04-20 11:43:24 -0600

    coorection progress

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -39,7 +39,7 @@ class Driver():
         left=left_distances
         right=right_distances
         
-        print(len(left))
+        print(len(left), len(right))
 
         left_max_distance=left[np.argmax(left)]
         right_max_distance=right[np.argmax(right)]
