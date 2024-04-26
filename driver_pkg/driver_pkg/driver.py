171ad6e1c2fc329cd35a29f2bf942a932f0bc6f6
srikanth <srikanth.popuri4444@gmail.com>
2024-04-26 15:14:33 -0600

    0.3 m no left

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -69,7 +69,7 @@ class Driver():
         print('steer between walls')
         left = self.filter.signal_smoothing_filter(left_distances[12:18])
         right = self.filter.signal_smoothing_filter(right_distances[12:18])
-        angle_matrix=np.array(range(10*6, 20*6,6))
+        angle_matrix=np.array(range(12*6, 18*6,6))
 
         distance=right*np.sin(np.deg2rad(angle_matrix))
 
