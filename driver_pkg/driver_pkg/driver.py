e6e2840167d7e74d037d04fae881dc6799efb953
srikanth <srikanth.popuri4444@gmail.com>
2024-04-26 14:54:20 -0600

    0.3 m no left

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -73,10 +73,10 @@ class Driver():
         print(angle_matrix)
 
         avg_left_distance = np.mean(left)
-        avg_right_distance = np.mean(distance)
+        avg_right_distance = np.mean(right)
 
-        #scaled_error = (avg_left_distance-avg_right_distance)/(avg_left_distance+avg_right_distance)
-        scaled_error = 0.3-avg_right_distance
+        scaled_error = (avg_left_distance-avg_right_distance)/(avg_left_distance+avg_right_distance)
+        #scaled_error = 0.3-avg_right_distance
         steering_gain = 1/180
         steering_angle = steering_gain*scaled_error
 
