6b79380bbb458c7009a913c30bc996fe69374d44
srikanth <srikanth.popuri4444@gmail.com>
2024-04-27 18:40:53 -0600

    0.3 m no left

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -54,8 +54,8 @@ class Driver():
     def scan_for_turn(self,left_distances,right_distances)-> int:
         
         self.throttle=0.5
-        left=self.filter.signal_smoothing_filter(left_distances[4:15])
-        right=self.filter.signal_smoothing_filter(right_distances[4:15])
+        left=self.filter.signal_smoothing_filter(left_distances[3:15])
+        right=self.filter.signal_smoothing_filter(right_distances[3:15])
         front_right=self.filter.signal_smoothing_filter(right_distances[0:5])
         front_left=self.filter.signal_smoothing_filter(left_distances[0:5])
 
