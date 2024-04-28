efb981c80c9e09b318867901b6879d97ced55724
srikanth <srikanth.popuri4444@gmail.com>
2024-04-28 15:47:04 -0600

    clipping distance 6.0

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -73,7 +73,7 @@ class Driver():
 
 
         print(left_max_distance,right_max_distance,front_right_max_distance)
-        if( right_max_distance>=left_max_distance and right_max_distance>=4.5):
+        if( right_max_distance>=left_max_distance and right_max_distance>=6):
                 e=10+np.argmax(right)*6
                 #print(-e)
                 return -e
@@ -81,7 +81,7 @@ class Driver():
         #           e=np.argmax(front_right)*6
         #          #print(-e)
         #           return -e
-        elif( left_max_distance>right_max_distance and left_max_distance>=4.5):
+        elif( left_max_distance>right_max_distance and left_max_distance>=6):
                  e=30+np.argmax(left)*6
                  #print(-e)
                  return e
