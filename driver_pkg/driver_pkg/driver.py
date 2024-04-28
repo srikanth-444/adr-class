ea5738aeaf1f3e57ecb0e0ac8783bb007980789c
srikanth <srikanth.popuri4444@gmail.com>
2024-04-27 19:09:09 -0600

    0.3 m no left

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -72,10 +72,10 @@ class Driver():
         front_left_max_distance=max(front_left)
 
 
-        #print(left_max_distance,right_max_distance)
+        print(left_max_distance,right_max_distance,front_right_max_distance)
         if( right_max_distance>=left_max_distance and right_max_distance>=4.5 and right_max_distance>=front_right_max_distance):
                 e=30+ np.argmax(right)*6
-                print(-e)
+                #print(-e)
                 return -e
         elif( front_right_max_distance>=front_left_max_distance and front_right_max_distance>=4):
                   e=np.argmax(front_right)*6
