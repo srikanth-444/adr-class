9b924d1cb3b3b072dd355145e13a198866a4502f
aidanluz <82617788+aidanluz@users.noreply.github.com>
2024-04-29 20:26:55 -0600

    Add files via upload

---
+++ stopsign_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'stopsign_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aidan',
+    maintainer_email='ailu9881@colorado.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drive=stopsign_pkg.drivernode:main'
+        ],
+    },
+)
---
+++ stopsign_pkg/stopsign_pkg/stopsignnode.py
@@ -0,0 +1,95 @@
+import rclpy
+from rclpy.node import Node
+import numpy as np
+from driver_pkg.driver import Driver
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+
+from sensor_msgs.msg import Image
+from deepracer_interfaces_pkg.msg import ServoCtrlMsg
+
+from cv_bridge import CvBridge, CvBridgeError
+
+    
+    
+class StopSign(Node):
+
+    def __init__(self):
+        super().__init__('StopSign')
+
+        ###?????
+        self.camera_message_sub_cb_grp = ReentrantCallbackGroup()
+        #client for configuring lidar
+        self.lidar_client=self.create_client(LidarConfigSrv,"/sensor_fusion_pkg/configure_lidar")
+        while not self.lidar_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = LidarConfigSrv.Request()
+
+
+        #lidar data subscriber
+        self.image_subcriber= self.create_subscription(Image, '/camera_pkg/display_mjpeg',self.camera_listen,10,callback_group=self.camera_message_sub_cb_grp)
+        
+        # steering publisher
+        self.steering_publisher= self.create_publisher(ServoCtrlMsg,'/ctrl_pkg/servo_msg',1)
+
+        timer_period = 0.1
+
+        self.timer = self.create_timer(timer_period, self.drive_timer_callback)
+
+        #initial values of steering and throttle
+        self.angle=0.0
+        self.throttle=0.0
+        self.stopsign = StopSign()
+        self.bridge = CvBridge()
+        self.flag=0.0
+
+
+    def camera_listen(self, msg):
+
+        try:
+            image = self.bridge.imgmsg_to_cv2(msg,"rgb8")
+        except CvBridgeError as e:
+            print(e)
+
+        self.driver.get_controls(image)
+        self.throttle=self.driver.get_throttle()
+        self.flag=self.driver.get_flag()
+    
+
+    def drive_timer_callback(self):
+        msg = ServoCtrlMsg()   
+                                                    
+        msg.angle= self.angle 
+        msg.throttle= self.throttle
+        self.steering_publisher.publish(msg)
+        self.get_logger().info("message published throttle: %f flag: %f"%(msg.throttle,float(self.flag)))
+     
+        
+       
+
+           
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    stopsign = StopSign()
+    try:
+        stopsign.get_logger().info('ready to execute')
+        executor = MultiThreadedExecutor()
+        rclpy.spin(stopsign,executor)
+    except KeyboardInterrupt:
+        msg = ServoCtrlMsg()   
+        stopsign.angle=0.0
+        stopsign.throttle=0.0                                      
+        msg.angle= stopsign.angle 
+        msg.throttle= stopsign.throttle
+        stopsign.steering_publisher.publish(msg)
+        stopsign.get_logger().info("message published steering : %f throttle: %f" %(msg.angle,msg.throttle))
+
+        stopsign.destroy_node()
+        rclpy.shutdown()
+    
+
+if __name__ == '__main__':
+    main()
