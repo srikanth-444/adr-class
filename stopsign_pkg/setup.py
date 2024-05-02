dc4f8c5eb8c6adbf7b7e1d25d803cd718bde9443
aidanluz <82617788+aidanluz@users.noreply.github.com>
2024-05-02 00:16:10 -0600

    Update setup.py

--- stopsign_pkg/setup.py
+++ stopsign_pkg/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'drive=stopsign_pkg.drivernode:main'
+            'stopsign=stopsign_pkg.stopsignnode:main'
         ],
     },
 )
