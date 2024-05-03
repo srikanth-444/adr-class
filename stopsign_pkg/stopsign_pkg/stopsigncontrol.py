8a626ded0c7998749cf0428cfd68fd308d9d7e51
aidanluz <82617788+aidanluz@users.noreply.github.com>
2024-05-02 19:50:24 -0600

    Update stopsigncontrol.py

--- stopsign_pkg/stopsign_pkg/stopsigncontrol.py
+++ stopsign_pkg/stopsign_pkg/stopsigncontrol.py
@@ -67,16 +67,16 @@ class StopSignControl():
         image[:,:,1] = red_layer
         image[:,:,2] = red_layer
         image = 255-image
-        for i in range(10):
-            image = cv2.GaussianBlur(image,(15,15),cv2.BORDER_DEFAULT)
+        # for i in range(10):
+        #     image = cv2.GaussianBlur(image,(15,15),cv2.BORDER_DEFAULT)
 
-        red_layer = image[:, :, 0]
+        # red_layer = image[:, :, 0]
         
-        active = red_layer < 150
-        red_layer[active] = 0
-        image[:,:,0] = red_layer
-        image[:,:,1] = red_layer
-        image[:,:,2] = red_layer
+        # active = red_layer < 150
+        # red_layer[active] = 0
+        # image[:,:,0] = red_layer
+        # image[:,:,1] = red_layer
+        # image[:,:,2] = red_layer
         
         # Setup SimpleBlobDetector parameters.
         params = cv2.SimpleBlobDetector_Params()
