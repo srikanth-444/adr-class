e5a01b986ed55b345009479b3560c5f2b9830504
aidanluz <82617788+aidanluz@users.noreply.github.com>
2024-05-02 20:34:28 -0600

    Update stopsigncontrol.py

--- stopsign_pkg/stopsign_pkg/stopsigncontrol.py
+++ stopsign_pkg/stopsign_pkg/stopsigncontrol.py
@@ -59,9 +59,9 @@ class StopSignControl():
         green_layer = image[:, :, 1]
         blue_layer = image[:, :, 2]
         
-        red_threshold = 50
-        green_threshold = 100
-        blue_threshold = 100
+        red_threshold = 150
+        green_threshold = 255
+        blue_threshold = 255
         active = (red_layer>red_threshold) * (green_layer < green_threshold) * (blue_layer < blue_threshold)
         red_layer[~active] = 0
         image[:,:,0] = red_layer
