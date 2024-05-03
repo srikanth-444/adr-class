adc849342ee1374afd857e570e7892feeb910183
srikanth <srikanth.popuri4444@gmail.com>
2024-05-03 12:12:42 -0600

    tunning

--- driver_pkg/driver_pkg/driver.py
+++ driver_pkg/driver_pkg/driver.py
@@ -212,8 +212,8 @@ class Driver():
         
     def steer_between_walls(self,left_distances,right_distances):
         #print('steer between walls')
-        left = left_distances[45:120]
-        right =right_distances[45:120]
+        left = left_distances[30:120]
+        right =right_distances[30:120]
         angle_matrix=np.array(range(45, 120,1))
 
         right_x= np.clip(right *np.sin(np.deg2rad(angle_matrix)),0.1,self.in_wall)
