0e38231ec2eafc8b8f35c467080cca2216c3f55b
aidanluz <82617788+aidanluz@users.noreply.github.com>
2024-05-04 14:05:18 -0600

    Update EKF.py

--- ekf_slam/ekf_slam/EKF.py
+++ ekf_slam/ekf_slam/EKF.py
@@ -43,14 +43,14 @@ class EKF():
         mu1_bar = self.bicycle_model(u1,dt)
 
         #calculate jacobian of bicycle model function
-        Gt = np.eye(3)
+        Gt = np.eye(3,3)
         Gt[0,2] = -v*np.sin(theta)*dt
         Gt[1,2] = v*np.cos(theta)*dt
         
         Sigma1_bar = Gt*self.Sigma*Gt.T + Rt
 
         #Sensor model is identity, Jacbian is identity
-        Ht = np.eye(3)
+        Ht = np.eye(3,3)
 
         #find kalman gain
         Kt = Sigma1_bar*Ht.T*np.linalg.inv(Ht*Sigma1_bar*Ht.T+Qt)
@@ -61,7 +61,7 @@ class EKF():
 
         #Perform correction step on the mean and covariance of the state
         self.mu = mu1_bar + Kt*(z1 - mu1_bar)
-        self.Sigma = (np.eye(3) - Kt*Ht)*Sigma1_bar
+        self.Sigma = (np.eye(3,3) - Kt*Ht)*Sigma1_bar
 
         self.state_history.append(self.mu)
         self.cov_history.append(self.Sigma)
@@ -71,7 +71,8 @@ class EKF():
     def observation(self,point_cloud):
 
         if(self.prev_point_cloud.size == 0):
-            self.prev_point_cloud = point_cloud+np.ones([point_cloud.shape[0],point_cloud.shape[1]])/10
+            self.prev_point_cloud = point_cloud
+            self.prev_point_cloud[self.prev_point_cloud==0] = 0.001
 
         # Create point cloud objects
         pc_fix = PointCloud(self.prev_point_cloud, columns=["x", "y", "z"])
