808d356b9e306456022291e2dec972c935c0cb38
aidanluz <82617788+aidanluz@users.noreply.github.com>
2024-05-04 18:38:46 -0600

    Update EKF.py

--- ekf_slam/ekf_slam/EKF.py
+++ ekf_slam/ekf_slam/EKF.py
@@ -69,6 +69,8 @@ class EKF():
         #Perform correction step on the mean and covariance of the state
         self.mu = mu1_bar + np.matmul(Kt,(z1 - mu1_bar))
         self.Sigma = np.matmul((np.eye(3,3) - np.matmul(Kt,Ht)),Sigma1_bar)
+        print(z1-mu1_bar)
+        print(np.matmul(Kt,(z1 - mu1_bar))
         print(self.mu)
 
         self.state_history.append(self.mu)
