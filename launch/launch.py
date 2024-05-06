dd60475bf28266c8d678eca3b19e50a3be048fb0
srikanth <srikanth.popuri4444@gmail.com>
2024-05-06 13:25:36 -0600

    launch

--- launch/launch.py
+++ launch/launch.py
@@ -4,22 +4,22 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='driver_pkg',
-            executable='drive',
-            name='driver',
-            output='screen'
-        ),
-        Node(
-            package='ekf_slam',
-            executable='ekf_slam',
-            name='slam',
-            output='screen'
-        ),
-        Node(
             package='stopsign_pkg',
             executable='stopsign',
             name='stop',
             output='screen'
            
+        ),
+        Node(
+            package='driver_pkg',
+            executable='drive',
+            name='driver',
+            output='screen'
+        ),   
+        Node(
+            package='ekf_slam',
+            executable='ekf_slam',
+            name='slam',
+            output='screen'
         )
     ])
